b94487b275e30f62b6b896caf558337aefc6c47e
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-08-23 12:51:08 +0530

    made auth slice for the login

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,29 +1,81 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { fetchWrapper } from '../utils/fetchWrapper'
+import { endpoints } from "../utils/constants"
+
+const initialState = {
+  isLoggedIn: false,
+  loading: false,
+  errors: {
+    loginError: "",
+    signupError: ""
+  }
+}
 
 
 export const authSlice = createSlice({
   name: 'auth',
-  initialState: {
-    isLoggedIn: false,
-  },
+  initialState: initialState,
   reducers: {
     setIsLoggedIn: (state, action) => {
       state.isLoggedIn = action.payload
+    },
+
+    setLoginError: (state, action) => {
+      state.errors.loginError = action.payload
+    },
+
+    setSignupError: (state, action) => {
+      state.errors.signupError = action.payload
+    },
+
+    setLoading: (state, action) => {
+      state.loading = action.payload
     }
+
   }
 })
 
 
-export const { setIsLoggedIn } = authSlice.actions
+export const { setIsLoggedIn, setLoginError, setSignupError, setLoading } = authSlice.actions
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const loginAsync = (username, password) => dispatch => {
-  // return fetch()
+export const loginAsync = (username, password) => async dispatch => {
 
-  return null
+  dispatch(setLoading(true))
+
+  try {
+    const res = await fetchWrapper.post(endpoints.LOGIN_USER, {
+      "username": username,
+      "password": password
+    })
+
+    const resData = await res.json()
+
+    if (res.ok) {
+
+      localStorage.setItem('refresh_token', resData?.access)
+      localStorage.setItem('access_token', resData?.refresh)
+
+      dispatch(setIsLoggedIn(true))
+
+    }
+
+    if (res.status === 401) {
+      dispatch(setLoginError(resData?.detail))
+    }
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+  finally {
+    setLoading(false)
+  }
+
+
 }
 
 
@@ -32,5 +84,8 @@ export const loginAsync = (username, password) => dispatch => {
 // in the slice file. For example: `useSelector((state) => state.login.value)`
 // export const selectUserData = state => state.login.userDetails;
 export const selectIsLoggedIn = state => state.auth.isLoggedIn
+export const selectLoginError = state => state.auth.errors.loginError
+export const selectSignupError = state => state.auth.errors.signupError
+export const selectLoading = state => state.auth.loading
 
 export default authSlice.reducer
