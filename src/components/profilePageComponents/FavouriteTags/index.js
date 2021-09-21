76277f18245fc086a7314e060908c67b3666fcb8
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-09-21 18:10:47 +0530

    now we can create new tags

--- src/components/profilePageComponents/FavouriteTags/index.js
+++ src/components/profilePageComponents/FavouriteTags/index.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectIsEditMode } from '../../../slices/userSlice'
 import Tag from "../../utilComponents/Tag"
 import AsyncSelect from '../../formComponents/AsyncSelect'
 import Button from '../../utilComponents/Button'
+import CreateTagModal from './CreateTagModal'
 
 function FavouriteTags({ activeTags, setActiveTags }) {
 
   const isEditMode = useSelector(selectIsEditMode)
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
 
 
   function handleSubmit(tag) {
-
-    console.table(tag)
-
     setActiveTags(prev => ([...prev, { ...tag, active: true }]))
-
   }
 
   function handleFilter(results) {
@@ -27,37 +25,40 @@ function FavouriteTags({ activeTags, setActiveTags }) {
   }
 
   return (
-    <div>
-      <div className="flex justify-between px-3">
-        <h2>Favourite Tags</h2>
-        <Button variant="outline-primary" type="button" className="text-sm" > Create new </Button>
-      </div>
-      <div className="flex flex-wrap">
+    <>
+      <div>
+        <div className="flex justify-between px-3">
+          <h2>Favourite Tags</h2>
+          <Button variant="outline-primary" type="button" className="text-sm" onClick={() => setIsCreateModalOpen(true)} > Create new </Button>
+        </div>
+        <div className="flex flex-wrap">
 
-        {activeTags.map(tag => {
+          {activeTags.map(tag => {
 
-          const onTagClick = () => {
-            if (!isEditMode) return;
+            const onTagClick = () => {
+              if (!isEditMode) return;
 
-            setActiveTags(prev => {
+              setActiveTags(prev => {
 
-              return [...prev].map(t => {
-                if (t.id === tag.id) {
-                  return { ...tag, active: !t.active }
-                }
+                return [...prev].map(t => {
+                  if (t.id === tag.id) {
+                    return { ...tag, active: !t.active }
+                  }
 
-                return t
+                  return t
+                })
               })
-            })
-          }
+            }
 
-          return <Tag key={tag.id} tag={tag} faded={!tag.active} onClick={onTagClick} />
-        })}
+            return <Tag key={tag.id} tag={tag} faded={!tag.active} onClick={onTagClick} />
+          })}
 
-        {isEditMode && <AsyncSelect onSubmit={handleSubmit} resultFilter={handleFilter} disabled={!isEditMode} />}
+          {isEditMode && <AsyncSelect onSubmit={handleSubmit} resultFilter={handleFilter} disabled={!isEditMode} />}
 
+        </div>
       </div>
-    </div>
+      <CreateTagModal show={isCreateModalOpen} onClose={() => setIsCreateModalOpen(false)}  />
+    </>
   )
 }
 
