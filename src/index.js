4441004193a93ef3ea225baf58b7eec760addc88
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-07 14:14:38 +0530

    added sentry-sdk and currently setup locally

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import * as sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing"
+
+sentry.init({
+  dsn:process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
