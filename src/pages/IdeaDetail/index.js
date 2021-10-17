5f9f1d5dc288cd2471df7740d8d96c923d28a675
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-17 08:34:12 +0530

    made IdeaDetail page

--- src/pages/IdeaDetail/index.js
+++ src/pages/IdeaDetail/index.js
@@ -1,22 +1,76 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { selectLoading, selectCurrentIdea } from "../../slices/ideaSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { selectLoading, selectCurrentIdea, getSingleIdeaAsync } from "../../slices/ideaSlice"
+import Main from '../../components/utilComponents/Main'
+import ContentContainer from '../../components/utilComponents/ContentContainer'
+import NewsContainer from '../../components/utilComponents/NewsContainer'
+import Sidebar from '../../components/Sidebar'
+import Header from '../../components/Header'
+import Tag from "../../components/utilComponents/Tag"
+
 
 function IdeaDetail() {
 
   const { ideaId } = useParams()
   const isLoading = useSelector(selectLoading)
   const idea = useSelector(selectCurrentIdea)
+  const dispatch = useDispatch()
 
-  console.log(isLoading);
+  useEffect(() => {
+
+    dispatch(getSingleIdeaAsync(ideaId))
+
+  }, [dispatch, ideaId])
 
 
   return (
-    <div>
-      {idea && (
-        <span>this is idea detail{ideaId}</span>
-    )}
+    <div className="flex bg-gray-100 dark:bg-gray-900 transition duration-500" >
+      <Sidebar />
+
+
+      <Main>
+        <Header />
+        <div className="flex divide-x divide-gray-50 overflow-hidden pt-2 " >
+          {idea && (
+            <ContentContainer className="flex-col space-y-1 overflow-auto keep-scrolling px-2 py-3 ml-2 bg-white rounded-l min-h-screen">
+              {/* <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" ></div> */}
+
+              <h1 className="text-xl font-bold" >{idea.title}</h1>
+
+              <div className="flex" >
+                {idea?.tags?.map(t => {
+
+                  return <Tag tag={t} key={t.id} />
+                })}
+              </div>
+              <p className="text-gray-600 py-2   ">
+                {idea.description}
+              </p>
+
+              <p className="py-2" >
+                {idea.body}
+              </p>
+
+              <ul>
+                <h2 className="text-lg font-bold" >Milestones</h2>
+                {idea?.milestones?.map((m, index) => {
+
+                  return <li className="" key={`${m[0]}--key-${index}`} >{m[1]}</li>
+                })}
+              </ul>
+
+
+            </ContentContainer>
+          )}
+          <NewsContainer className="mr-2 bg-white rounded-r min-h-screen" >
+            {/* <div className="h-48 p-4 rounded shadow-lg bg-white dark:bg-gray-800"></div> */}
+            csdcnskjdn
+          </NewsContainer>
+        </div>
+      </Main>
+
+      {/* <ThemeChangeFAB /> */}
     </div>
   )
 }
