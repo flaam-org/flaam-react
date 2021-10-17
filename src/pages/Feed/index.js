5ef2549875c38a01fd9ed9fb00ca766d4281c2e5
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-17 08:59:40 +0530

    removed Header and Sidebar from the Feed component

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from '../../components/Header'
-import Sidebar from '../../components/Sidebar'
-import Main from '../../components/utilComponents/Main'
 import ContentContainer from '../../components/utilComponents/ContentContainer'
 import NewsContainer from '../../components/utilComponents/NewsContainer'
 
@@ -9,40 +6,31 @@ import NewsContainer from '../../components/utilComponents/NewsContainer'
 function Feed() {
 
   return (
-    <div className="flex bg-gray-100 dark:bg-gray-900 transition duration-500" >
-      <Sidebar />
-
-
-      <Main>
-        <Header />
-        <div className="flex divide-x divide-gray-50/40  overflow-hidden pt-2" >
-          <ContentContainer className="flex-col space-y-1 overflow-auto keep-scrolling pb-10 px-2">
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-            <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-            </div>
-          </ContentContainer>
-          <NewsContainer className="px-2" >
-            <div className="h-48 p-4 rounded shadow-lg bg-white dark:bg-gray-800">
-            </div>
-          </NewsContainer>
+    <div className="flex divide-x divide-gray-50/40  overflow-hidden pt-2" >
+      <ContentContainer className="flex-col space-y-1 overflow-auto keep-scrolling pb-10 px-2">
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
         </div>
-      </Main>
-
-      {/* <ThemeChangeFAB /> */}
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
+        </div>
+      </ContentContainer>
+      <NewsContainer className="px-2" >
+        <div className="h-48 p-4 rounded shadow-lg bg-white dark:bg-gray-800">
+        </div>
+      </NewsContainer>
     </div>
+
   )
 }
 
