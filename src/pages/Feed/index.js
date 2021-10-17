23b9a61f66e19eed95fe58f0f8e36de47bcc1b44
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-17 13:07:31 +0530

    added idea cards to feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import IdeaCard from '../../components/IdeaCard'
 import ContentContainer from '../../components/utilComponents/ContentContainer'
 import NewsContainer from '../../components/utilComponents/NewsContainer'
+import { getFeedAsync, selectFeed } from '../../slices/feedSlice'
 
 
 function Feed() {
 
+  const feed = useSelector(selectFeed)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getFeedAsync())
+  },[dispatch])
+
   return (
     <div className="flex divide-x divide-gray-50/40  overflow-hidden pt-2" >
-      <ContentContainer className="flex-col space-y-1 overflow-auto keep-scrolling pb-10 px-2">
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
-        <div className="h-48 p-3 rounded shadow-lg w-full flex-shrink-0 bg-white dark:bg-gray-800" >
-        </div>
+      <ContentContainer className="flex-col space-y-3 overflow-auto keep-scrolling py-5 mx-2 bg-white">
+
+      {feed.map(idea => {
+
+        return <IdeaCard idea={idea} key={idea.id} />
+      })}
+
       </ContentContainer>
       <NewsContainer className="px-2" >
         <div className="h-48 p-4 rounded shadow-lg bg-white dark:bg-gray-800">
