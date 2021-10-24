03d070f1ba523d7f6339107c464090a52bbc6e36
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-10-24 23:54:20 +0530

    using the CustomTab

--- src/components/profilePageComponents/TabsContainer/index.js
+++ src/components/profilePageComponents/TabsContainer/index.js
@@ -1,33 +1,18 @@
 import { Tab } from '@headlessui/react'
 import React from 'react'
-import { joinClassNames } from '../../../utils/functions'
+import CustomTab from '../../utilComponents/CustomTab'
 import ImplementationTabPanel from '../ImplementationTabPanel'
 import MyIdeasTabPanel from '../MyIdeasTabPanel'
 import SavedTabPanel from '../SavedTabPanel'
-
-const TTab = ({ children }) => {
-  return (
-    <Tab
-
-      className={({ selected }) =>
-        joinClassNames(
-          'w-full py-2 capitalize leading-5 focus:outline-none select-none transition-colors duration-150 ease-in-out',
-          selected
-            ? 'bg-transparent border-b-2 border-blue-500 text-green-600 font-bold '
-            : 'text-green-400 hover:text-green-400/90 border-b-2 border-transparent hover:border-blue-500/60 '
-        )
-      }>{children}</Tab>
-  )
-}
 
 function TabsContainer() {
   return (
     <Tab.Group as="div" className="w-full px-2 py-3 sm:px-0 snap-start "  >
       <Tab.List className="flex py-1 px-2 space-x-2 rounded-md border-b">
 
-        <TTab>Saved</TTab>
-        <TTab>My Ideas</TTab>
-        <TTab>My Implementations</TTab>
+        <CustomTab>Saved</CustomTab>
+        <CustomTab>My Ideas</CustomTab>
+        <CustomTab>My Implementations</CustomTab>
 
 
       </Tab.List>
